4a0d3276c006a09c391959666d771e20bf2af8e1
shassen092 <shassen092@gmail.com>
2023-11-29 08:06:54 +0100

    Adding metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
@@ -6,13 +7,27 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 import CircularColorsDemo from '@/components/CircularColorsDemo';
 
+const getBlogPost = React.cache(async (slug) => {
+  return await loadBlogPost(slug);
+});
+
+export const generateMetadata = async ({ params }) => {
+  const {
+    frontmatter: { title, abstract },
+  } = await getBlogPost(params.postSlug);
+  return {
+    title: title,
+    description: abstract,
+  };
+};
+
 async function BlogPost() {
   const headersList = headers();
-  const pathname = headersList.get('x-invoke-path') || '';
+  const pathname = headersList.get('x-invoke-path').slice(1) || '';
   const {
     frontmatter: { title, publishedOn },
     content,
-  } = await loadBlogPost(pathname);
+  } = await getBlogPost(pathname);
 
   return (
     <article className={styles.wrapper}>
